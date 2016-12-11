ff6c27e1c36a14d714b725c656d9c19592f0bc23
cy59n05y5 <juny1905@naver.com>
2016-12-11 23:45:04 +0900

    Final

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var app = express();
 app.set("view engine", 'ejs');
 app.use(express.static(__dirname + '/public'));
 
+app.get('/', function(req, res) {
+    res.redirect("/index.htm");
+});
+
 app.get('/hello', function(req, res) {
     res.render('my_first_ejs', { name: req.query.nameQuery });
 });

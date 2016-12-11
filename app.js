1fc1dc7aa42771486a5de4738e2506b998092140
cy59n05y5 <juny1905@naver.com>
2016-12-11 23:30:46 +0900

    Example 5

--- app.js
+++ app.js
@@ -1,34 +1,17 @@
 var express = require('express');
-var path = require('path');
 var app = express();
 
 app.set("view engine", 'ejs');
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res) {
-    res.render('my_first_ejs');
+app.get('/hello', function(req, res) {
+    res.render('my_first_ejs', { name: req.query.nameQuery });
 });
+
+app.get('/hello/:nameParam', function(req, res) {
+    res.render('my_first_ejs', { name: req.params.nameParam });
+})
 
 app.listen(8000, function() {
     console.log('Server On!');
-})
-
-var data = { count: 0 };
-
-app.get('/', function(req, res) {
-    data.count++;
-    res.render('my_first_ejs', data);
-});
-
-app.get('/reset', function(req, res) {
-    data.count = 0;
-    res.render('my_first_ejs', data);
-});
-app.get('/set/count', function(req, res) {
-    if (req.query.count) data.count = req.query.count;
-    res.render('my_first_ejs', data);
-})
-app.get('/set/:num', function(req, res) {
-    data.count = req.query.count;
-    res.render('my_first_ejs', data);
 })
